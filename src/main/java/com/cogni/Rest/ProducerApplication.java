f6983983ed58a6f8d2fab3e84b8988f8b0d5b9e7
729485 <729485@PC386399.cts.com>
2018-12-21 18:42:02 +0530

    ribbon1

--- src/main/java/com/cogni/Rest/ProducerApplication.java
+++ src/main/java/com/cogni/Rest/ProducerApplication.java
@@ -3,12 +3,29 @@ package com.cogni.Rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RibbonClient(name = "employee", configuration = SimpleCalculatorRibbonConfiguration.class)
 public class ProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
 	}
+	
+	@Configuration
+	public class MyConfig {
+
+		@Bean
+		@LoadBalanced
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+	}
 }
+
